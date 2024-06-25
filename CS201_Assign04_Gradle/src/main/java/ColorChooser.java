ed74601d17b5bd21da02cfec32e778777267e93f
smorales1994 <123330769+smorales1994@users.noreply.github.com>
2024-06-24 22:22:38 -0400

    Delete CS201_Assign04_Gradle directory

--- CS201_Assign04_Gradle/src/main/java/ColorChooser.java
+++
@@ -1,7 +0,0 @@
-import java.awt.Color;
-
-// Interface for "color chooser" objects which compute a color
-// based on an iteration count
-public interface ColorChooser {
-	public Color getColor(int iterCount);
-}
